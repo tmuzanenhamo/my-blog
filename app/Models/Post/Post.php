ece056495bf9790bd7ed92a6acaf1b0c50c8f7d4
tmuzanenhamo <tawandamuzanenhamo14@gmail.com>
2021-10-17 11:40:17 +0200

    add eloquent post model with a belongs to relationship with category

---
+++ app/Models/Post/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Post;
+
+use App\Models\Category\Category;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * category
+     * returns the relationship between a post and a category
+     *
+     * @return BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
